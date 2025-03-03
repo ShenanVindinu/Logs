f9e09a80c29bb1c5d89b789567cbe3451569235b
ShenanVindinu <shenanvindinu@gmail.com>
2025-03-03 10:28:35 +0530

    Initial commit

---
+++ src/test/java/lk/ijse/gdse/shenan/logs/LogsApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse.shenan.logs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
