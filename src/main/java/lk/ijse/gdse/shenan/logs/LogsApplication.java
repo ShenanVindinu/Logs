ca461f480c44ff4a6bc6bef461b6a301cc20c90d
ShenanVindinu <shenanvindinu@gmail.com>
2025-03-03 10:29:31 +0530

    initial commit

--- src/main/java/lk/ijse/gdse/shenan/logs/LogsApplication.java
+++ src/main/java/lk/ijse/gdse/shenan/logs/LogsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LogsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LogsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LogsApplication.class, args);
+    }
 
 }
